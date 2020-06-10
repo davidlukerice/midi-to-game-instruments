24dcb0381fb802ff8a6d52371abe2233ddf0241c
David Rice <davidlukerice@gmail.com>
2020-06-09 20:26:17 -0500

    Add react

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # midi-to-keypress
-App for converting midi to key presses
+
+App for converting midi to key presses. Built with Electron and React.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">[MIDI-to-keypress here]</header>
+    </div>
+  );
+}
+
+export default App;
